9dd7fbfc87ad80337a744dbb75a0b6b7692feb51
mezevetova <mezevetova.nata@yandex.ru>
2016-11-15 02:20:55 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML (categories) {
       var homeHtmlToInsertIntoMainPage = insertProperty(
         homeHtml, 
         "randomCategoryShortName", 
-       chosenCategoryShortName
+        "'" + chosenCategoryShortName.short_name + "'"
       );
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
